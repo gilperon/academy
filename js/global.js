15332c1a04a9f05d0c0b783eeb742da783087708
Gustavo <your_email@example.com>
2024-05-11 15:44:11 -0300

    Ajustes

--- js/global.js
+++ js/global.js
@@ -2,11 +2,18 @@ console.log('Start');
 
 $(document).ready(function() {
     // Add click event listener to all cancel href elements ul.navbar-nav li a
+    //$('ul.navbar-nav li a').click(function(e) { // ativa para todos , o atual ativa apenas pro menu-concursos
     $('ul.navbar-nav li a#menu-cursos').click(function(e) {
         e.preventDefault(); 
         $('ul.navbar-nav li a').removeClass('active');
         $(this).addClass('active');
     });
+
+    $('ul.navbar-nav li a').not('#navbarDropdown').click(function(e) {
+        e.preventDefault(); 
+        window.scrollTo(0,document.body.scrollHeight);
+    });
+
 
 
     $('#flipButton').on('click', function() {
