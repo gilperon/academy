50cbc59b7650eb16f2b46be2b06fb4fee2e9369a
Gustavo <your_email@example.com>
2024-05-11 10:46:57 -0300

    Paginas do site

--- js/global.js
+++ js/global.js
@@ -7,4 +7,48 @@ $(document).ready(function() {
         $('ul.navbar-nav li a').removeClass('active');
         $(this).addClass('active');
     });
+
+
+    $('#flipButton').on('click', function() {
+        $('#flipSVG').toggleClass('flipped');
+      });
+
+      const observer = new MutationObserver(mutationsList => {
+        mutationsList.forEach(mutation => {
+          if (mutation.type === 'attributes') {
+            const flipButton = $('#flipButton');
+            const flipSVG = $('#flipSVG');
+            if (!flipButton.hasClass('show')) {
+              flipSVG.removeClass('flipped');
+            } else {
+              flipSVG.addClass('flipped');
+            }
+
+            const flipButtonMobile = $('#flipButtonMobile');
+            const flipSVGMobile = $('#flipSVGMobile');
+            if (!flipButtonMobile.hasClass('show')) {
+                flipSVGMobile.removeClass('flipped');
+              } else {
+                flipSVGMobile.addClass('flipped');
+              }
+
+            const dropCursos = $('#dropCursos');
+            if (!dropCursos.hasClass('show')) {
+              $('#navbarDropdown').removeClass('active');
+            } else {
+              $('#navbarDropdown').addClass('active');
+            }
+          }
+        });
+      });
+
+      observer.observe(document.getElementById('flipButton'), { attributes: true });
+      observer.observe(document.getElementById('flipButtonMobile'), { attributes: true });
+      observer.observe(document.getElementById('dropCursos'), { attributes: true });
+    
+
+
+
 });
+
+
