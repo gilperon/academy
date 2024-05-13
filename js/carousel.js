6df9e1192e76ed37ea30c3e0dacd85249f21ead8
Gustavo <your_email@example.com>
2024-05-13 20:59:25 -0300

    Ajustes carousel

--- js/carousel.js
+++ js/carousel.js
@@ -61,5 +61,41 @@ $( document ).ready(function() {
     });
 
 
+    $('#slider-cursos-1').owlCarousel({
+        margin: 15,
+        nav: true,
+        loop: true,
+        dots: false,
+        navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding:40,
+            },
+            768: {
+                items: 3,
+            }
+        }
+    });
+
+
+    $('#slider-cursos-2').owlCarousel({
+        margin: 15,
+        nav: true,
+        loop: true,
+        dots: false,
+        navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding:40,
+            },
+            768: {
+                items: 3,
+            }
+        }
+    });
+
+
 
 });
