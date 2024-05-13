7e1dd65fd0517f77ee91e3bc71446683ed0843e0
Gustavo <your_email@example.com>
2024-05-13 20:28:01 -0300

    Ajuste

---
+++ js/carousel.js
@@ -0,0 +1,65 @@
+
+
+$( document ).ready(function() {
+
+
+    $('#slider-testimonial').owlCarousel({
+        margin: 15,
+        nav: true,
+        loop: true,
+        dots: true,
+        navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding:40,
+            },
+            768: {
+                items: 2,
+            }
+            ,
+            980: {
+                items: 2,
+            }
+        }
+    });
+
+
+    $('#slider-docente').owlCarousel({
+        margin: 15,
+        nav: true,
+        loop: true,
+        dots: false,
+        navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding:40,
+            },
+            768: {
+                items: 3,
+            }
+        }
+    });
+
+
+    $('#slider-especialidades').owlCarousel({
+        margin: 15,
+        nav: true,
+        loop: true,
+        dots: false,
+        navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding:40,
+            },
+            768: {
+                items: 2,
+            }
+        }
+    });
+
+
+
+});
