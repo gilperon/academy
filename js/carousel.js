54f42b8a0a1a2e8f97c2b84bf24f3194e2907c6c
Gustavo <your_email@example.com>
2024-05-14 11:56:18 -0300

    Ajuste carousel

--- js/carousel.js
+++ js/carousel.js
@@ -70,10 +70,14 @@ $( document ).ready(function() {
         responsive: {
             0: {
                 items: 1,
-                stagePadding:40,
+                stagePadding:50,
+            },
+            568: {
+                items: 2,
             },
             768: {
                 items: 3,
+                stagePadding:60,
             }
         }
     });
@@ -88,13 +92,57 @@ $( document ).ready(function() {
         responsive: {
             0: {
                 items: 1,
-                stagePadding:40,
+                stagePadding:50,
+            },
+            568: {
+                items: 2,
             },
             768: {
                 items: 3,
+                stagePadding:60,
             }
         }
     });
+
+
+
+
+
+    //Coloca first-active e last-active no carousel
+    $('.owl-carousel').trigger('to.owl.carousel', 0);
+    function brandSliderClasses() {
+        $('.owl-carousel').each(function() {
+            var total = $(this).find('.owl-item.active').length;
+            $(this).find('.owl-item').removeClass('first-active');
+            $(this).find('.owl-item').removeClass('last-active');
+            $(this).find('.owl-item.active').each(function(index) {
+                if (index === 0) {
+                    $(this).addClass('first-active')
+                }
+                if (index === total - 1 && total > 1) {
+                    $(this).addClass('last-active')
+                }
+            })
+        })
+    }
+    brandSliderClasses();
+    $('.owl-carousel').on('translated.owl.carousel', function(event) {
+        brandSliderClasses()
+    });
+    //Clica em um vai para a primeira posição
+    $(document).on('click', '.owl-item', function() {
+        let carousel = $(this).closest('.owl-carousel');
+        let index = $(this).index();
+        let relativeIndex = carousel.data('owl.carousel').relative(index);
+        carousel.trigger('to.owl.carousel', [relativeIndex, 300]);
+    });
+
+
+
+
+
+
+
 
 
 
